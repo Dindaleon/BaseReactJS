0c97992a051a439891c303b7e6c4aa6e28ff91a6
Dindaleon <gixty7@gmail.com>
2015-07-16 18:07:17 -0430

    Working on 404 page

---
+++ dev/public/views/404.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+module.exports = React.createClass({
+
+  displayName: '404 Page Not Found',
+
+  render: function render() {
+
+    return (
+      <div id='pageNotFound'>
+        <h1>{this.props.name}404 Page Not Found</h1>
+        <h6>We are sorry but the page you were looking for could not be found!</h6> 
+        <div onClick={() => this.goBack()}>Go back</div>       
+      </div>
+    );
+  }
+});
--- dev/routes.jsx
+++ dev/routes.jsx
@@ -1,12 +1,14 @@
-import { Route, DefaultRoute } from "react-router";
+import { Route, DefaultRoute, NotFoundRoute } from "react-router";
 import React from "react";
 
 import App from "./public/views/app";
+import pageNotFound from "./public/views/404";
 import Account from "./public/views/account";
 import Users from "./public/views/users";
 export default (
   <Route path='/' handler={App}>
     <DefaultRoute name='account' handler={Account}/>
-		<Route name='users' handler={Users}/>    
+		<Route name='users' handler={Users}/>
+		<NotFoundRoute handler={pageNotFound}/>
   </Route>
 );
